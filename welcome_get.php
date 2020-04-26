1af6902730401d1b8dee86a10ee105f9d168e8b8
arnoldT1000 <54363499+arnoldT1000@users.noreply.github.com>
2020-04-26 08:27:59 -0400

    Add files via upload

---
+++ welcome_get.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<?php
+
+
+if (($_GET["name"] == "demo") && ($_GET["email"] == "demo1@example.com")) {
+    echo "Have a good day Demo!";
+} else {
+    echo "Wrong name or email!";
+}
+?>
+
+
+</body>
+</html>
