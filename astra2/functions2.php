10da00a129bfe31de1fafbce3ceb8d178d9d8ccd
arnoldT1000 <54363499+arnoldT1000@users.noreply.github.com>
2021-03-22 09:45:38 -0400

    Add files via upload

--- astra2/functions2.php
+++ astra2/functions2.php
@@ -1,162 +1,35 @@
 <?php
-/**
- * Astra functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package Astra
- * @since 1.0.0
- */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+function load_stylesheets()
+{
+	wp_register_style('stylesheet', get_template_directory_uri() .'/css/bootstrap.min.css', array(), false, 'all');
+	wp_enqueue_style('stylesheet');
+
+	wp_register_style('style', get_template_directory_uri() .'/css/style.css', array(), false, 'all');
+	wp_enqueue_style('style');
 }
 
-/**
- * Define Constants
- */
-define( 'ASTRA_THEME_VERSION', '3.0.1' );
-define( 'ASTRA_THEME_SETTINGS', 'astra-settings' );
-define( 'ASTRA_THEME_DIR', trailingslashit( get_template_directory() ) );
-define( 'ASTRA_THEME_URI', trailingslashit( esc_url( get_template_directory_uri() ) ) );
+add_action('wp_enqueue_scripts', 'load_stylesheets');
 
 
-/**
- * Minimum Version requirement of the Astra Pro addon.
- * This constant will be used to display the notice asking user to update the Astra addon to the version defined below.
- */
-define( 'ASTRA_EXT_MIN_VER', '3.0.0' );
-
-/**
- * Setup helper functions of Astra.
- */
-require_once ASTRA_THEME_DIR . 'inc/core/class-astra-theme-options.php';
-require_once ASTRA_THEME_DIR . 'inc/core/class-theme-strings.php';
-require_once ASTRA_THEME_DIR . 'inc/core/common-functions.php';
-
-/**
- * Update theme
- */
-require_once ASTRA_THEME_DIR . 'inc/theme-update/class-astra-theme-update.php';
-require_once ASTRA_THEME_DIR . 'inc/theme-update/astra-update-functions.php';
-require_once ASTRA_THEME_DIR . 'inc/theme-update/class-astra-theme-background-updater.php';
-require_once ASTRA_THEME_DIR . 'inc/theme-update/class-astra-pb-compatibility.php';
-
-
-/**
- * Fonts Files
- */
-require_once ASTRA_THEME_DIR . 'inc/customizer/class-astra-font-families.php';
-if ( is_admin() ) {
-	require_once ASTRA_THEME_DIR . 'inc/customizer/class-astra-fonts-data.php';
+function include_jquery()
+{
+	wp_deregister_script('jquery');
+	wp_register_style('jquery', get_template_directory_uri() .'/js/jquery-3.6.0.min.js', '', 1, true);
+	add_action('wp_enqueue_scripts', 'jquery');
 }
 
-require_once ASTRA_THEME_DIR . 'inc/customizer/class-astra-fonts.php';
+add_action('wp_enqueue_scripts', 'include_jquery');
 
-require_once ASTRA_THEME_DIR . 'inc/core/class-astra-walker-page.php';
-require_once ASTRA_THEME_DIR . 'inc/core/class-astra-enqueue-scripts.php';
-require_once ASTRA_THEME_DIR . 'inc/core/class-gutenberg-editor-css.php';
-require_once ASTRA_THEME_DIR . 'inc/class-astra-dynamic-css.php';
-
-/**
- * Custom template tags for this theme.
- */
-require_once ASTRA_THEME_DIR . 'inc/core/class-astra-attr.php';
-require_once ASTRA_THEME_DIR . 'inc/template-tags.php';
-
-require_once ASTRA_THEME_DIR . 'inc/widgets.php';
-require_once ASTRA_THEME_DIR . 'inc/core/theme-hooks.php';
-require_once ASTRA_THEME_DIR . 'inc/admin-functions.php';
-require_once ASTRA_THEME_DIR . 'inc/core/sidebar-manager.php';
-
-/**
- * Markup Functions
- */
-require_once ASTRA_THEME_DIR . 'inc/markup-extras.php';
-require_once ASTRA_THEME_DIR . 'inc/extras.php';
-require_once ASTRA_THEME_DIR . 'inc/blog/blog-config.php';
-require_once ASTRA_THEME_DIR . 'inc/blog/blog.php';
-require_once ASTRA_THEME_DIR . 'inc/blog/single-blog.php';
-/**
- * Markup Files
- */
-require_once ASTRA_THEME_DIR . 'inc/template-parts.php';
-require_once ASTRA_THEME_DIR . 'inc/class-astra-loop.php';
-require_once ASTRA_THEME_DIR . 'inc/class-astra-mobile-header.php';
-
-/**
- * Functions and definitions.
- */
-require_once ASTRA_THEME_DIR . 'inc/class-astra-after-setup-theme.php';
-
-// Required files.
-require_once ASTRA_THEME_DIR . 'inc/core/class-astra-admin-helper.php';
-
-require_once ASTRA_THEME_DIR . 'inc/schema/class-astra-schema.php';
-
-if ( is_admin() ) {
-
-	/**
-	 * Admin Menu Settings
-	 */
-	require_once ASTRA_THEME_DIR . 'inc/core/class-astra-admin-settings.php';
-	require_once ASTRA_THEME_DIR . 'inc/lib/notices/class-astra-notices.php';
-
-	/**
-	 * Metabox additions.
-	 */
-	require_once ASTRA_THEME_DIR . 'inc/metabox/class-astra-meta-boxes.php';
+function loadjs()
+{
+	wp_register_style('customjs', get_template_directory_uri() .'/js/scripts.js', '', 1, true);
+	wp_enqueue_script('customjs');
 }
 
-require_once ASTRA_THEME_DIR . 'inc/metabox/class-astra-meta-box-operations.php';
-
-
-/**
- * Customizer additions.
- */
-require_once ASTRA_THEME_DIR . 'inc/customizer/class-astra-customizer.php';
-
-
-/**
- * Compatibility
- */
-require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-jetpack.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/woocommerce/class-astra-woocommerce.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/edd/class-astra-edd.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/lifterlms/class-astra-lifterlms.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/learndash/class-astra-learndash.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-beaver-builder.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-bb-ultimate-addon.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-contact-form-7.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-visual-composer.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-site-origin.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-gravity-forms.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-bne-flyout.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-ubermeu.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-divi-builder.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-amp.php';
-require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-yoast-seo.php';
-require_once ASTRA_THEME_DIR . 'inc/addons/transparent-header/class-astra-ext-transparent-header.php';
-require_once ASTRA_THEME_DIR . 'inc/addons/breadcrumbs/class-astra-breadcrumbs.php';
-require_once ASTRA_THEME_DIR . 'inc/addons/heading-colors/class-astra-heading-colors.php';
-require_once ASTRA_THEME_DIR . 'inc/builder/class-astra-builder-loader.php';
-
-// Elementor Compatibility requires PHP 5.4 for namespaces.
-if ( version_compare( PHP_VERSION, '5.4', '>=' ) ) {
-	require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-elementor.php';
-	require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-elementor-pro.php';
-}
-
-// Beaver Themer compatibility requires PHP 5.3 for anonymus functions.
-if ( version_compare( PHP_VERSION, '5.3', '>=' ) ) {
-	require_once ASTRA_THEME_DIR . 'inc/compatibility/class-astra-beaver-themer.php';
-}
-
-/**
- * Load deprecated functions
- */
-require_once ASTRA_THEME_DIR . 'inc/core/deprecated/deprecated-filters.php';
-require_once ASTRA_THEME_DIR . 'inc/core/deprecated/deprecated-hooks.php';
-require_once ASTRA_THEME_DIR . 'inc/core/deprecated/deprecated-functions.php';
+add_action('wp_enqueue_scripts', 'loadjs');
 
 add_theme_support('menus');
+add_theme_support('post-thumbnails');
+
+register_nav_menus(array('top-menu' => __('Top Menu', 'theme'),'footer-menu' => __('Footer Menu', 'theme')));
