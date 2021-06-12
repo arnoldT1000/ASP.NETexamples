c4463926aee146f11f7cb373ab39720633e67896
arnoldT1000 <54363499+arnoldT1000@users.noreply.github.com>
2021-06-12 11:26:58 -0400

    Create footer.php

---
+++ themeA/footer.php
@@ -0,0 +1,30 @@
+
+        <!-- footer -->
+			<footer class="w3-container w3-blue w3-padding-64 w3-center w3-opacity">
+				
+					
+                    
+						<h2>For Reservations: <span>1-800 123 45 67</span></h2>
+						Designed by <a rel="nofollow" href="http://tech888.infinityfreeapp.com/" target="_blank">Leading Experts</a> 
+					
+                    
+					              
+		        
+			</footer>
+<!-- / footer -->
+		
+        <?php wp_footer();?>
+
+</body>
+</html>
+
+	
+	
+
+
+
+
+
+
+
+
